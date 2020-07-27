3e3b9b837f1ce003410e214005e4ffc4d87b5241
unknown <tbrito495@gmail.com>
2020-07-27 11:48:27 -0700

    atividade finlizada

---
+++ semana3/variaveis/js-vanilla-template/src/index.js
@@ -0,0 +1,41 @@
+// Exercicios de escrita
+// 1-
+//  a) imprime o valor de b que é igual a 10
+//  b) impre o valor de a e b (5,10)
+// 2- imprime os valores (10,10,10)
+// 
+// Exercicios de escrita de código
+// 1-
+nome =  null
+idade = null
+console.log(typeof(nome, idade))
+// retornou object, pq se trata de um item mas este nao tem valor definido.
+nome = prompt ("Qual é seu nome?")
+idade = prompt ("Qual é a sua idade?")
+console.log(nome, idade)
+console.log(typeof(nome, idade))
+resultado foi uma string, pq represta um texto
+console.log("Olá, " + nome +" você tem " + idade + " anos de idade")
+// 2-
+nome = prompt("1- Qual é o seu nome?")
+endereço = prompt("2-Qual é o seu endereço?")
+idade = prompt("3-Qual a sua idade?")
+apelido = prompt("4- Qual o seu apelido de infancia?")
+hobbie = prompt("5-Qual o seu hobbie?")
+console.log(nome, endereço, idade, apelido, hobbie)
+// 3-
+let array = ["pizza", "lasagna", "bife", "macarrão", "sorvete"]
+console.log(array[0], array[1], array[2], array[3], array[4])
+console.log("Essas são as minhas comidas preferidas")
+console.log(array[0])
+console.log(array[1])
+console.log(array[2])
+console.log(array[3])
+console.log(array[4])
+// 4-
+let array = [ "Você está de chinelo?", "Você viajou esse ano?", "Você dirige?"]
+let rp = [ "false", "false", "true"]
+console.log(array[0], rp[0])
+console.log(array[1], rp[1])
+console.log(array[2], rp[2])
+
