4de59ce4b1408f62e2b720585107485249f62fec
unknown <tbrito495@gmail.com>
2020-08-03 12:54:26 -0700

    Atividade Incompleta

---
+++ semana4/funcoes/js-vanilla-template/src/index.js
@@ -0,0 +1,52 @@
+// Exercicios de leitura de codigo
+
+// Exercicio 1
+// a- 10 e 50
+// b- realizaria a função, mas nao apareceria nada no console.
+
+// Exercicio 2
+// a- Darvas. Goli
+// b- Amanda, Caio
+
+// Exercicio 3
+// A função checa se os valores dentro de Array são pares, se forem, serão multiplicados pelo mesmo valor e devolvidas na arrayFinal. 
+// Um possivel nome seria arrayPotenciaçao, ja que os valores serão resultados de multplicações por si mesmos.
+
+// Exercicio 4
+// a-
+// function info(){
+//     console.log("Eu sou Thiago, tenho 28 anos, e estou aprendendo javascript")
+// }
+
+// info()
+// b-
+// function info (nome, cidade, idade, estudante){
+//     if (estudante === true){
+//         console.log(`Meu nome é ${nome}, tenho ${idade} anos, sou de ${cidade} e sou estudante`)
+//     }else if (estudante === false) {
+//         console.log(`Meu nome é ${nome}, tenho ${idade} anos, sou de ${cidade} e não sou estudante`)
+//     }
+// }
+// let final = info("Thiago", "São josé dos Quatro Marcos", 28, true)
+
+//Exercicio 5
+// a-
+// function soma(numero1, numero2){
+//     const soma = numero1 + numero2
+//     console.log(soma)
+// }
+// soma(3, 4)
+// b-
+    // function funcao2(){
+    //     if(numero1 >= numero2){
+    //         console.log(true)
+    //     }else if(numero1<=numero2)
+    //         console.log(false)
+    //     }  
+    // let numero1 = 3
+    // let numero2= 2
+    // funcao2()
+// c-
+
+ 
+  
