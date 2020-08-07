acc5fadd63c2c4e33c139e2914090a789a1e4574
unknown <tbrito495@gmail.com>
2020-08-07 12:54:15 -0700

    Exercicios de interpretação de código completos

---
+++ semana4/lista-de-exercicios/js-vanilla-template/src/index.js
@@ -0,0 +1,21 @@
+// Exercicios de interpretação de código
+// 1-
+// O codigo é um programa que faz o cambio do dolar.
+// Ele o faz através de uma função, que gera um prompt onde o usuario vai dar o valor da cotação do dolar guardando este valor na const cotacao.
+// Por fim o valor final e obtido pela função conversorDeMoeda que foi chamada pela const MeuDinheiro, onde posteriormente e impresso o valor final.
+// O valor impresso sera o valor que o usuario colocou multiplicado por 100.
+
+// 2-
+// O codigo e um programa que calcula a rentabilidade de um investimento
+// Ele o faz através de uma função que chama uma condicional que ira calcular o rendimento de acordo com o investimento escolhido.
+// valores impressos: novoMontante = 165, segundoMontante = retorna alert de erro
+
+// 3-
+// O codigo e composta por uma array com alguns numeros e 2 arrays vazias, onde e feito um loop que distribui os numeros pares para a array1 e impares para array2 e por fim imprime o tamanho das arrays.
+// Esta distribuiçao e feita pelo loop for of.
+// 1º console.log = 14, 2ºconsole.log = 6, 3º console.log = 8
+
+// 4-
+// o codigo busca dentro da array o menor valor e o maior valor através do loop for of
+// 1º console.log = -10, 2ºconsole.log = 1590
+
