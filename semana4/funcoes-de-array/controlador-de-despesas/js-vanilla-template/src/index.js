d5b45dde7dc211c02bd09c44e231bfc08e972729
unknown <tbrito495@gmail.com>
2020-08-06 18:21:36 -0700

    etapas 2 e 3 realizadas

--- semana4/funcoes-de-array/controlador-de-despesas/js-vanilla-template/src/index.js
+++ semana4/funcoes-de-array/controlador-de-despesas/js-vanilla-template/src/index.js
@@ -1,15 +1,25 @@
 function despesaInput(){
     const valorInput = document.getElementById("valor")
-    const tipo = document.getElementsByClassName("tipo")
+    const tipo = document.getElementById("tipo")
     const descricao = document.getElementById("descricao")
     
     const novaDespesa= {
         valor: valorInput.value,
         tipo: tipo.value,
         descricao: descricao.value,
+        
     }
-    
+    console.log(novaDespesa)
     valorInput.value=""
     tipo.value = ""
     descricao.value = ""
+
+    ListaDeDespesas = []
+    ListaDeDespesas.push(novaDespesa)
+    console.log(ListaDeDespesas )
+
+    if (valorInput.value !== "" && tipo.value !== ""  && descricao.value !== "") {
+    } else {
+        alert("Por favor preencha todos os campos")
+    }
 }
