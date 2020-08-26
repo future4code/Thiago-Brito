cd98ce8ac5f236d6566a5ba70f36f45a3e0d081d
unknown <tbrito495@gmail.com>
2020-08-25 19:12:44 -0700

    atividade aula 25 incompleta

--- semana7/aula25/labenusers/src/App.js
+++ semana7/aula25/labenusers/src/App.js
@@ -4,27 +4,88 @@ import './App.css';
 
 export default class App extends React.Component {
  state = {
-   nome:[],
-   email:[],
-   listaUsuario: ""
+   listaUsuario: [],
+
  };
 
-  createUsers = () => {
-  const body ={
-    name:this.state.nome,
-    name:this.state.email
-  };
 
-  const request = axios.post(
-    "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
+  listarUsuario = () => {
+    const request = axios.get(
+       `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users` ,
+      {
+        headers: {
+          Authorization: "Thiago-Brito-Jackson"
+        }
+      }
+      
+    );
+    request
+      .then((resposta) => {
+        this.setState({ listaUsuario: resposta.data});
+        console.log(resposta.data)
+      })
+      .catch((erro)=>{
+        console.log("Ocorreu um erro")
+      });
+      console.log(request)
+  };
+  createUsers = (nome, email) => {
+    const body ={
+      name:nome,
+      email:email
+    };
+
+    const request = axios.post(
+      `https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users`,
     body,
     {
       headers:{
-        authorization: "Thiago-Brito-Jackson"
+        Authorization: "Thiago-Brito-Jackson"
       }
     }
   );
+  request
+    .then((resposta)=> {
+      console.log(resposta)
+      alert(`Usuário ${resposta} foi criado com sucesso`)
+      this.listaUsuario();
+      this.setState({nomeUsuario:""});
+    })
+    .catch((erro) =>{
+      console.log("erro");
+    });
+    console.log(request)
+  };
+  componentDidMount(){
+    this.listarUsuario();
+  }
+  onChangeInputUsuario = (evento) => {
+    this.setState({name:evento.target.value});
+  }
+  onChangeInputEmail = (evento) => {
+    this.setState({email:evento.target.value});
+  }
+  render(){
+    const criarUsuario = () => {
+      this.createUsers(this.state.name, this.state.email)
+    }
+    return(
+        <div className="App">
+          <h1>Insira usuario</h1>
+          <input value={this.nomeUsuario} onChange={(event)=>this.onChangeInputUsuario(event)}/>
+          <input value={this.emailUsuario} onChange={(event)=>this.onChangeInputEmail(event)}/>
+          <button onClick={criarUsuario}>criar</button>
+          <h2>usuarios</h2>
+          {this.state.listaUsuario.map((item) =>{
+            return(
+            <div>
+              <p key={item.id}>{item.nome}</p>
+              </div>)
+            })}
+        </div>
+      );
+      }
+  };
+  
 
-}
-}
 
--- semana7/aula25/labenusers/src/components/ListaDeUsuarios.js
+++ semana7/aula25/labenusers/src/components/ListaDeUsuarios.js
@@ -1,23 +1,23 @@
-export class UsersList extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {};
-    }
+// export class UsersList extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {};
+//     }
   
-    render() {
-      return (
-        <UserList>
-          <h2>Usuários Cadastrados:</h2>
-          {this.props.users.map(user => (
-            <UserListItem key={user.id}>
-              {user.name}{" "}
-              <DeleteButton onClick={() => this.props.onDeleteUser(user.id)}>
-                X
-              </DeleteButton>
-            </UserListItem>
-          ))}
-        </UserList>
-      );
-    }
-  }
+//     render() {
+//       return (
+//         <UserList>
+//           <h2>Usuários Cadastrados:</h2>
+//           {this.props.users.map(user => (
+//             <UserListItem key={user.id}>
+//               {user.name}{" "}
+//               <DeleteButton onClick={() => this.props.onDeleteUser(user.id)}>
+//                 X
+//               </DeleteButton>
+//             </UserListItem>
+//           ))}
+//         </UserList>
+//       );
+//     }
+//   }
   
