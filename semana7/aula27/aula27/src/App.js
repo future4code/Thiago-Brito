2adaf91fe2b510311c1d1fd2ce62908e8512db2c
unknown <tbrito495@gmail.com>
2020-08-27 20:02:41 -0700

    Ajustes no codigo + estilização

--- semana7/aula27/aula27/src/App.js
+++ semana7/aula27/aula27/src/App.js
@@ -1,15 +1,52 @@
 import React from 'react';
 import logo, { ReactComponent } from './logo.svg';
 import './App.css';
-// import styled from styled.Components
-import axios from "axios"
+import styled from "styled-components";
+import axios from "axios";
+import {Button, Input} from '@material-ui/core'
 
+
+const Imagem = styled.img`
+max-height:30vh;
+max-width:20vw;
+`
+const Logo = styled.img`
+max-height:25vh;
+`
+const Text = styled.p`
+font-weight:bold;
+`
+const WrapperDiv = styled.div`
+display:grid;
+grid-template-columns:1fr 1fr 1fr;
+grid-template-rows:1fr 1fr 1fr;
+flex-direction:column;
+height:100vh;
+`
+const LogoDiv = styled.div`
+display:grid;
+grid-row:1;
+grid-column:2;
+`
+const ContentDiv = styled.div`
+display:grid;
+grid-row:2;
+grid-column:2;
+align-self:center;
+
+`
+const ButtonDiv = styled.div`
+display:grid;
+grid-row:3;
+grid-column:2;
+width:10vw;
+height: 5vh;
+
+`
 export default class App extends React.Component {
 state ={
-
   name:"",
-  birthday:"",
-  img: "",
+  img:"",
   status: "",
   nickname: "",
 }
@@ -23,30 +60,34 @@ randomCharacter = () =>{
     console.log(response)
     this.setState({
       name: response.data[0].name,
-      birthday: response.data[0].birthday,
       img: response.data[0].img,
       status: response.data[0].status,
-      nickname: response.data[0].nickname
+      nickname: response.data[0].nickname,
     })
   })  
   .catch((error)=> {
     console.log(error)
   })
-
 }
   render(){
 
     console.log(this.state)  
     return (
-      <div className="App">
-       <h1>Gerar Personagem Aleatorio</h1>
-       <img src={this.state.img}/>
-       <p>{this.state.name}</p>
-       <p>{this.state.birthday}</p>
-       <p>{this.state.nickname}</p>
-       <p>{this.state.status}</p>
-       <button onClick={this.randomCharacter}>Novo Personagem</button>
-      </div>
+      <WrapperDiv>
+        <LogoDiv>
+        <Logo src="img/walterwhite.png" alt=""></Logo>
+        </LogoDiv>
+        <ContentDiv>
+       <h1>Breaking Bad Random Character Generator</h1>
+       <Imagem src={this.state.img}></Imagem>
+       <p><strong>Name:</strong>  {this.state.name}</p>
+       <p><strong>Nickname:</strong> {this.state.nickname}</p>
+        <p><strong>Status:</strong> {this.state.status}</p>
+        </ContentDiv>
+        <ButtonDiv>
+          <Button variant="contained" color="secondary" onClick={this.randomCharacter}>New Character</Button>
+        </ButtonDiv>
+       </WrapperDiv>
     );
   }
   }
